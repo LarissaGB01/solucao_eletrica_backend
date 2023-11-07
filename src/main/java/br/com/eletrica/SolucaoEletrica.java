18b9707cb9f85d567f3bb3a8e949eb44bd83d71b
Larissa Barcelos <larissa.barcelos@sempreceub.com>
2023-11-07 17:51:45 -0300

    adiciona configuracoes de ambiente

--- src/main/java/br/com/eletrica/SolucaoEletrica.java
+++ src/main/java/br/com/eletrica/SolucaoEletrica.java
@@ -1,5 +1,6 @@
 package br.com.eletrica;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SolucaoEletrica {
 
 	public static void main(String... args) {
+		Dotenv.configure().load();
+
 		SpringApplication.run(SolucaoEletrica.class, args);
 	}
 }
