62ff650a9789ed9af4e7ca67753a25ad39f04029
Larissa Barcelos <larissa.barcelos@sempreceub.com>
2023-11-07 19:39:15 -0300

    retira .env

--- src/main/java/br/com/eletrica/SolucaoEletrica.java
+++ src/main/java/br/com/eletrica/SolucaoEletrica.java
@@ -1,6 +1,5 @@
 package br.com.eletrica;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SolucaoEletrica {
 
 	public static void main(String... args) {
-		Dotenv.configure().load();
-
 		SpringApplication.run(SolucaoEletrica.class, args);
 	}
 }
